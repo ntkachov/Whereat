8db86ff3b2a02adea03c055f675ec9fb14877538
Anna Lee <leeanna@student.umass.edu>
2012-04-10 16:33:58 -0400

    new android project

---
+++ app/src/com/whereat/app/WhereatActivity.java
@@ -0,0 +1,13 @@
+package com.whereat.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhereatActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
