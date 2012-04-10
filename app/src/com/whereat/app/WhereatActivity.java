913958f8d593ec023576285008026c29e3c12bcc
ntkachov <ntkachov@ntkachov-Dimension-8250.(none)>
2012-04-10 16:43:59 -0400

    Added simple webview to the app

--- app/src/com/whereat/app/WhereatActivity.java
+++ app/src/com/whereat/app/WhereatActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class WhereatActivity extends Activity {
+
+	WebView mWebView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	mWebView = (WebView) findViewById(R.id.webview);
+	mwebView.loadURL("http://www.thedailynerd.com");
     }
 }
