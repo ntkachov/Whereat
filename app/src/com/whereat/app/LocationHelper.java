07e49aff49bf4e1a92e6fa54c34adada7fe52f8c
Anna Lee <leeanna@student.umass.edu>
2012-04-10 23:39:58 -0400

    basic location updates

---
+++ app/src/com/whereat/app/LocationHelper.java
@@ -0,0 +1,62 @@
+package com.whereat.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class LocationHelper {
+
+	private Context context;
+
+	public LocationHelper(Context context) {
+		this.context = context.getApplicationContext();
+		getLocationUpdates();
+	}
+
+	private List<LocationUpdateListener> listeners = new ArrayList<LocationUpdateListener>();
+
+	public interface LocationUpdateListener {
+		public void onLocationUpdate(Location location);
+	}
+
+	public void registerForUpdates(LocationUpdateListener listener) {
+		listeners.add(listener);
+	}
+
+	private void getLocationUpdates() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location
+				// provider.
+				for (LocationUpdateListener listener : listeners) {
+					listener.onLocationUpdate(location);
+				}
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			public void onProviderEnabled(String provider) {
+			}
+
+			public void onProviderDisabled(String provider) {
+			}
+		};
+
+		// Register the listener with the Location Manager to receive location
+		// updates
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, locationListener);
+	}
+}
